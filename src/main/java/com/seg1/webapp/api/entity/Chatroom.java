8ed8708fb825dbeef2aa0a354f6c0264674b5a44
Steven Berdak <stevenberdak@gmail.com>
2025-04-26 13:18:49 -0700

    start user list impl

--- src/main/java/com/seg1/webapp/api/entity/Chatroom.java
+++ src/main/java/com/seg1/webapp/api/entity/Chatroom.java
@@ -1,27 +1,41 @@
 package com.seg1.webapp.api.entity;
 
-import org.hibernate.annotations.CreationTimestamp;
-
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
+
 import javax.validation.constraints.Size;
 
-import java.util.List;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "chatrooms")
 public class Chatroom {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @CreationTimestamp
     private LocalDateTime created_at;
 
-    @Column(unique=true)
-    @Size(min=5, message="Chatroom title must be at least 5 characters")
+    @Column(unique = true)
+    @Size(min = 5, message = "Chatroom title must be at least 5 characters")
     private String title;
+
+    @Size(min = 5, message = "Chatroom title must be at least 5 characters")
     private String header;
+
+    @Column(length = 7)
+    private String color;
+
+    private List<String> tags;
 
     @OneToMany(mappedBy = "id")
     private List<Participant> participants;
@@ -32,20 +46,25 @@ public class Chatroom {
     public List<Message> getMessages() {
         return messages;
     }
+
     public void setMessages(List<Message> messages) {
         this.messages = messages;
     }
+
     public LocalDateTime getCreated_at() {
         return created_at;
     }
+
     public void setCreated_at(LocalDateTime created_at) {
         this.created_at = created_at;
     }
+
     private String description;
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -53,25 +72,48 @@ public class Chatroom {
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getHeader() {
         return header;
     }
+
     public void setHeader(String header) {
         this.header = header;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
     public List<Participant> getParticipants() {
         return participants;
     }
+
     public void setParticipants(List<Participant> participant) {
         this.participants = participant;
-    } 
+    }
 }
