1c1bf3bcd5d60914bfce657c0b1a055fb097694e
Steven Berdak <stevenberdak@gmail.com>
2025-04-28 10:03:32 -0700

    fix annotation, remove Ping class

--- src/main/java/com/seg1/webapp/api/controller/ApiParticipantController.java
+++ src/main/java/com/seg1/webapp/api/controller/ApiParticipantController.java
@@ -1,13 +1,19 @@
 package com.seg1.webapp.api.controller;
 
-import com.seg1.webapp.api.entity.Participant;
-import com.seg1.webapp.api.repository.ParticipantRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.seg1.webapp.api.entity.Participant;
+import com.seg1.webapp.api.repository.ParticipantRepository;
 
 @RestController
 @RequestMapping("/api/participants")
@@ -15,7 +21,6 @@ public class ApiParticipantController {
 
     private final ParticipantRepository repo;
 
-    @Autowired
     public ApiParticipantController(ParticipantRepository repo) {
         this.repo = repo;
     }
